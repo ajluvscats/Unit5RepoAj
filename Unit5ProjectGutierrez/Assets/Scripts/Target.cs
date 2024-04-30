9f0e9babb9a2566faf53662841373a6daca3af2d
ajluvscats <aj_349@icloud.com>
2024-04-30 15:25:34 -0700

    ftsertgert

--- Unit5ProjectGutierrez/Assets/Scripts/Target.cs
+++ Unit5ProjectGutierrez/Assets/Scripts/Target.cs
@@ -1,23 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Build.Content;
 using UnityEngine;
 
 public class Target : MonoBehaviour
 {
     private Rigidbody targetRb;
+    private GameManager gameManager;
     private float minSpeed = 12;
     private float maxSpeed = 16;
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -6;
+    public int pointValue;
 
     // Start is called before the first frame update
     void Start()
     {
         targetRb = GetComponent<Rigidbody>();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+
 
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse); ;
+
         transform.position = RandomSpawnPos();
     }
 
@@ -41,6 +47,7 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
+        gameManager.UpdateScore(pointValue);
     }
     private void OnTriggerEnter(Collider other)
     {
